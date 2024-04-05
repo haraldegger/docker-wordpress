266d0c38f787446523ad27f99e21e81e9576f288
Harald Egger <haraldegger@gmail.com>
2024-04-06 00:12:48 +0200

    First commit which installs everything we need and provides a basic wordpress intance.

---
+++ srv/bin/001-prepare.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#-------------------------------------------------------------------------#
+echo "Installing applications..."
+export DEBIAN_FRONTEND="noninteractive"
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install nginx
+apt-get -y install mariadb-server
+apt-get -y install php8.2-fpm
+apt-get -y install php8.2-mysql
+apt-get -y install openssl
+apt-get -y install openssh-server
+apt-get -y install cron
+apt-get -y install nano #for convenience, when manually interventing on the system
+#-------------------------------------------------------------------------#
+echo "Executing mysql_secure_installation..."
+/etc/init.d/mariadb start
+mysql_secure_installation <<EOF
+
+n
+n
+y
+y
+y
+y
+EOF
+#-------------------------------------------------------------------------#
